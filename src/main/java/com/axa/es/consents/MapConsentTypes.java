6a1bbcbe112ac104b39a8340004fd9b850b07110
Ivan <ivancordonm@gmail.com>
2022-03-11 07:48:57 +0100

    next step verify consents

--- src/main/java/com/axa/es/consents/MapConsentTypes.java
+++ src/main/java/com/axa/es/consents/MapConsentTypes.java
@@ -2,11 +2,15 @@ package com.axa.es.consents;
 
 import com.axa.es.consents.model.Consent;
 import com.axa.es.consents.model.Consents;
+
+import org.springframework.stereotype.Service;
+
 import com.axa.es.consents.model.ConsentList;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class MapConsentTypes {
 
     public Consents mapTypes(ConsentList consentList) {
@@ -14,6 +18,7 @@ public class MapConsentTypes {
         List<Consent> mappedConsents = new ArrayList<>();
 
         for (String type : consentList.getConsentIds()) {
+            System.out.println("type: " + type);
             mappedConsents.add(new Consent(type, false));
         }
         return new Consents(mappedConsents);
--- src/main/java/com/axa/es/consents/VerifyConsents.java
+++ src/main/java/com/axa/es/consents/VerifyConsents.java
@@ -1,8 +1,12 @@
 package com.axa.es.consents;
 
 import com.axa.es.consents.model.Consents;
+
+import org.springframework.stereotype.Service;
+
 import com.axa.es.consents.model.Consent;
 
+@Service
 public class VerifyConsents {
 
     public boolean verifyConsents(Consents consents) {
