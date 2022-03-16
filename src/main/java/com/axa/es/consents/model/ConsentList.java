886bff1d00cc9377e2662a6ea360743fbf1e192a
Ivan <ivancordonm@gmail.com>
2022-03-16 14:58:44 +0100

    running

--- src/main/java/com/axa/es/consents/model/ConsentList.java
+++ src/main/java/com/axa/es/consents/model/ConsentList.java
@@ -2,10 +2,11 @@ package com.axa.es.consents.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class ConsentList {
+public class ConsentList implements Serializable {
 
     private List<String> consentIds;
 
